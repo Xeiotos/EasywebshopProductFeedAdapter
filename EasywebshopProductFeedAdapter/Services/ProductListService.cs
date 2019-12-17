773428c4cc340334711addd04fee5fed0f3668bd
Chris Soberon <chris.soberon.w5916@student.hogent.be>
2019-12-17 16:42:36 +0100

    Add ProductListService

---
+++ EasywebshopProductFeedAdapter/Services/ProductListService.cs
@@ -0,0 +1,32 @@
+﻿using EasywebshopProductFeedAdapter.Domain.Agents;
+using EasywebshopProductFeedAdapter.Domain.Feeds;
+using EasywebshopProductFeedAdapter.Net.Http;
+using EasywebshopProductFeedAdapter.Util.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasywebshopProductFeedAdapter.Services
+{
+    public class ProductListService
+    {
+
+        private readonly IAgent _agent;
+        private readonly HttpClientInstanceController _httpClientController;
+
+        public ProductListService(HttpClientInstanceController httpClientInstanceController, IAgent agent)
+        {
+            _httpClientController = httpClientInstanceController;
+            this._agent = agent;
+        }
+
+        public async Task<XmlFeed> GetProductListAsync()
+        {
+            var feedSerializer = new XMLFeedSerializer();
+            var response = await _httpClientController.Post(_agent);
+            string responseContent = await response.Content.ReadAsStringAsync();
+            return feedSerializer.Deserialize(responseContent);
+        }
+    }
+}
