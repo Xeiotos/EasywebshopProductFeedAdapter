6878126d23d0fb9b86a549a94778c3524c6bd6bc
Chris Soberon <chris.soberon.w5916@student.hogent.be>
2019-12-17 16:42:03 +0100

    Add FeedWriterService Stub

---
+++ EasywebshopProductFeedAdapter/Services/FeedWriterService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EasywebshopProductFeedAdapter.Domain.Feeds;
+
+namespace EasywebshopProductFeedAdapter.Services
+{
+    public class FeedWriterService
+    {
+        public void Write(XmlFeed feed)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
