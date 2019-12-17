d3d2f8e8c68e06f2a76abd1002d21df5ce5e38bc
Chris Soberon <chris.soberon.w5916@student.hogent.be>
2019-12-17 16:39:55 +0100

    Add Authorization types

---
+++ EasywebshopProductFeedAdapter/Models/Authorization/AuthorizationType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasywebshopProductFeedAdapter.Domain.Authorization
+{
+    public sealed class AuthorizationType
+    {
+        public static readonly AuthorizationType None = new AuthorizationType("None");
+        public static readonly AuthorizationType Basic = new AuthorizationType("Basic");
+        public static readonly AuthorizationType Bearer = new AuthorizationType("Bearer");
+
+        private AuthorizationType(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get; private set; }
+    }
+}
