57bfbd64037cc9c1bbc5d9bc9292cafc75ce857c
Chris Soberon <chris.soberon.w5916@student.hogent.be>
2019-12-17 16:37:55 +0100

    Add Base64Encode extension to String

---
+++ EasywebshopProductFeedAdapter/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasywebshopProductFeedAdapter.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToBase64(this String str)
+        {
+            var strBytes = System.Text.Encoding.UTF8.GetBytes(str);
+            return System.Convert.ToBase64String(strBytes);
+        }
+    }
+}
