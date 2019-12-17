61161b0d02a6dfd087a33b150965e3fa65304b4a
Chris Soberon <chris.soberon.w5916@student.hogent.be>
2019-12-17 16:43:25 +0100

    Add FeedSerializer Interface

---
+++ EasywebshopProductFeedAdapter/Util/Serialization/IFeedSerializer.cs
@@ -0,0 +1,15 @@
+﻿using EasywebshopProductFeedAdapter.Domain.Feeds;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasywebshopProductFeedAdapter.Util.Serialization
+{
+    public interface IFeedSerializer
+    {
+        void Serialize(IFeed src, Stream outputStream);
+        XmlFeed Deserialize(string inputString);
+    }
+}
