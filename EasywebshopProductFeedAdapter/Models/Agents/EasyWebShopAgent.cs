c60ed03bc445007fd4c251f6344ad72f4b86a9fc
Chris Soberon <chris.soberon.w5916@student.hogent.be>
2019-12-17 16:38:11 +0100

    Add Agent code

---
+++ EasywebshopProductFeedAdapter/Models/Agents/EasyWebShopAgent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EasywebshopProductFeedAdapter.Domain.Authorization;
+using EasywebshopProductFeedAdapter.Extensions;
+
+namespace EasywebshopProductFeedAdapter.Domain.Agents
+{
+    public class EasyWebShopAgent : IAgent
+    {
+        private string _username;
+        private string _password;
+        public string Token => (_username + ":" + _password).ToBase64();
+        public AuthorizationType AuthorizationType { get; }
+        public string ConnectionString { get; set; }
+
+
+        public EasyWebShopAgent()
+        {
+
+        }
+    }
+}
---
+++ EasywebshopProductFeedAdapter/Models/Agents/IAgent.cs
@@ -0,0 +1,15 @@
+﻿using EasywebshopProductFeedAdapter.Domain.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasywebshopProductFeedAdapter.Domain.Agents
+{
+    public interface IAgent
+    {
+        string Token { get; }
+        AuthorizationType AuthorizationType { get; }
+        string ConnectionString { get; set; }
+    }
+}
