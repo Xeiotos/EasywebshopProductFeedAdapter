c80b871a14aceec798fce8f73cbe52b9724205ce
Chris Soberon <chris.soberon.w5916@student.hogent.be>
2019-12-18 01:55:40 +0100

    Add SerializationExtensions and XmlExtensions

---
+++ EasywebshopProductFeedAdapter/Extensions/SerializationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace EasywebshopProductFeedAdapter.Extensions
+{
+    public static class SerializationExtensions
+    {
+        public static string SerializeObject<T>(this T toSerialize)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(toSerialize.GetType());
+
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, toSerialize);
+                return textWriter.ToString();
+            }
+        }
+    }
+}
---
+++ EasywebshopProductFeedAdapter/Extensions/XmlExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace EasywebshopProductFeedAdapter.Extensions
+{
+    public static class XmlExtensions
+    {
+        public static XmlNode GetChild(this XmlNode xmlNode, string name)
+        {
+            return xmlNode.ChildNodes?.Cast<XmlNode>().Where(n => n.Name == name).FirstOrDefault();
+        }
+    }
+}
