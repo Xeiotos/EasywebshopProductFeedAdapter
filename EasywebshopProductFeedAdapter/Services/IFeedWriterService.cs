c204519dff63d58b79c6e8873205eec00d20da22
Chris Soberon <chris.soberon.w5916@student.hogent.be>
2019-12-20 23:19:57 +0100

    Replace FeedFormatterService with FeedWriterService

---
+++ EasywebshopProductFeedAdapter/Services/IFeedWriterService.cs
@@ -0,0 +1,13 @@
+﻿using EasywebshopProductFeedAdapter.Domain.Feeds;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasywebshopProductFeedAdapter.Services
+{
+    public interface IFeedWriterService
+    {
+        public void Write(Feed feed);
+    }
+}
